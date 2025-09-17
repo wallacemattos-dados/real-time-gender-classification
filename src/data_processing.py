1333a41a9f7e1c0ee9d46d9de345f0fd77167ede
wally963 <wallacemattos5963@gmail.com>
2025-09-16 23:03:02 -0300

    feat(data-processing): adiciona função de preprocessamento de imagem

--- src/data_processing.py
+++ src/data_processing.py
@@ -20,3 +20,19 @@ def carregar_caminhos_imagens(caminho_pasta: str) -> List[str]:
     arquivos = [os.path.join(caminho_pasta, f) for f in os.listdir(caminho_pasta) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
     print(f"Total de imagens encontradas: {len(arquivos)}")
     return arquivos
+
+def preprocessar_imagem(caminho_imagem: str, tamanho: Tuple[int, int] = (200, 250)) -> np.ndarray:
+    """
+    Carrega e pré-processa uma imagem: redimensiona e normaliza os valores dos pixels para o intervalo [0, 1].
+
+    Args:
+        caminho_imagem (str): O caminho para a imagem a ser processada.
+        tamanho (tuple[int, int]): O tamanho para o qual a imagem deve ser redimensionada (largura, altura).
+
+    Returns:
+        np.ndarray: A imagem pré-processada como um array NumPy.
+    """
+    imagem = Image.open(caminho_imagem)
+    imagem = imagem.resize((tamanho_desejado [1], tamanho_desejado[0]))  
+    imagem_array = np.array(imagem) / 255.0  # Normaliza para [0, 1]
+    return imagem_array
